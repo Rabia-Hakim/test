1db2d158e665438cd6808e340848c37621f37c88
Hakim Rabia <74686376+Rabia-Hakim@users.noreply.github.com>
2022-10-25 10:57:38 +0200

    capturepy

---
+++ capture.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+googleChromeOptions = webdriver.chrome.options.Options()
+googleChromeOptions.headless = True
+googleChromeOptions.add_argument('--window-size=1280,720')
+googleChrome = webdriver.Chrome(executable_path="/usr/bin/chromedriver",
+options=googleChromeOptions)
+pageUrl = "https://www.w3schools.com";
+googleChrome.get(pageUrl)
+googleChrome.save_screenshot('images/w3schools_google-chrome.png')
+googleChrome.close()
