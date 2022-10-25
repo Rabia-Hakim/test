8adb35351ad437b69cfdfc1c1fa2f8434b8e0168
Hakim Rabia <74686376+Rabia-Hakim@users.noreply.github.com>
2022-10-25 11:03:02 +0200

    Update capture.py

--- capture.py
+++ capture.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
 googleChromeOptions = webdriver.chrome.options.Options()
 googleChromeOptions.headless = True
 googleChromeOptions.add_argument('--window-size=1280,720')
