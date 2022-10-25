57a56938cca6ef71b6b989b6432976bca15f4db0
Hakim Rabia <74686376+Rabia-Hakim@users.noreply.github.com>
2022-10-25 11:04:32 +0200

    Update capture.py

--- capture.py
+++ capture.py
@@ -3,11 +3,13 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-googleChromeOptions = webdriver.chrome.options.Options()
+googleChromeOptions = Options()
+googleChromeOptions.add_argument("--headless")
 googleChromeOptions.headless = True
 googleChromeOptions.add_argument('--window-size=1280,720')
-googleChrome = webdriver.Chrome(executable_path="/usr/bin/chromedriver",
-options=googleChromeOptions)
+driver = webdriver.Chrome(service=Service('/usr/bin/chromedriver'),
+                          options=chrome_options)
+
 pageUrl = "https://www.w3schools.com";
 googleChrome.get(pageUrl)
 googleChrome.save_screenshot('images/w3schools_google-chrome.png')
